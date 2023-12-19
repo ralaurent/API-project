7d1911a3f972d97be8325115b959ffd4ccd64679
Work Station <workstation@MacBook-Pro-34.local>
2023-12-18 20:36:55 -0500

    Add reviews redux

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,5 @@
+/** Action Type Constants: */
+export const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS';
+export const CREATE_REVIEW = 'reviews/CREATE_REVIEW';
+export const UPDATE_REVIEW = 'reviews/UPDATE_REVIEW';
+export const REMOVE_REVIEW = 'reviews/REMOVE_REVIEW';
