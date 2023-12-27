d0025d08df15f087b6fd875cd4d5fd8d8ad6647d
Work Station <workstation@MacBook-Pro-34.local>
2023-12-27 02:41:04 -0500

    Fixed minified error

--- frontend/src/components/pages/SingleSpot.jsx
+++ frontend/src/components/pages/SingleSpot.jsx
@@ -28,31 +28,30 @@ function SingleSpot(){
         findSpot()
     }, [])
 
-    useEffect(() => {
-        if(!spot?.SpotImages) return
-        const spotImages = Object.values(spot.SpotImages)
+    // useEffect(() => {
+    //     if(!spot?.SpotImages) return
+    //     const spotImages = Object.values(spot?.SpotImages)
+        
+    //     const preview = spotImages.find((spot) => spot?.preview === true)
+    //     const images = spotImages
+    //     .filter((data) => data?.preview === false)
+    //     .map((image) => image?.url)
+    //     // console.log(images)
+    //     setPreviewImage(preview?.url)
+    //     setImages(images)
+    // }, [spot])
+
+    if(spot?.SpotImages){
+        const spotImages = Object.values(spot?.SpotImages)
         
         const preview = spotImages.find((spot) => spot?.preview === true)
         const images = spotImages
         .filter((spot) => spot?.preview === false)
         .map((image) => image?.url)
-        // console.log(images)
+        
         setPreviewImage(preview?.url)
         setImages(images)
-    }, [spot])
-
-    // if(spot?.SpotImages){
-    //     const spotImages = Object.values(spot?.SpotImages)
-        
-    //     const preview = spotImages.find((spot) => spot?.preview === true)
-    //     const images = spotImages
-    //     .filter((spot) => spot?.preview === false)
-    //     .map((image) => image?.url)
-
-    //     console.log(images)
-    //     setPreviewImage(preview?.url)
-    //     setImages(images)
-    // }
+    }
 
     const closeMenu = () => setShowMenu(!showMenu);
 
